4c8fad8ded2159f46145667a1c8c1a4993c4d0cc
Narendra Harilal Talreja <NTalreja@slb.com>
2018-07-10 12:20:02 +0530

    edited service file

--- src/app/shared/modal/contactbook.service.ts
+++ src/app/shared/modal/contactbook.service.ts
@@ -15,38 +15,14 @@ export class ContactBookService {
   }
 
   create(contact) {
-    // console.log('contact - - - - - ', contact);
-    // console.log(this.http.post(this.url, JSON.stringify(contact)));
     return this.http.post(this.url, contact);
-    // return this.http.post(this.url, JSON.stringify(contact));
   }
 
   edit(contact) {
-    // return this.http.put(this.url + '/' + contact.id, JSON.stringify(contact));
-    console.log(contact);
-    console.log('okokokokokokokokok', contact.id);
     return this.http.put(this.url + '/' + contact.id, contact);
-    // return this.http.put(`${this.url}/${contact.id}`, contact);
   }
 
   delete(contact) {
     return this.http.delete(this.url + '/' + contact.id);
   }
-
-
-  // getContacts() {
-  //   return this.http.get(this.url);
-  // }
-
-  // createContact(contact) {
-  //   return this.http.post(this.url, JSON.stringify(contact));
-  // }
-
-  // updateContact(contact) {
-  //   return this.http.put(this.url + '/' + contact.id, JSON.stringify(contact));
-  // }
-
-  // deleteContact(id) {
-  //   return this.http.delete(this.url + '/' + id);
-  // }
 }
